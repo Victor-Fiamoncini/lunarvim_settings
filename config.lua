42fe3dc6ac48a1b4ef6f6b436f8e513fcee4ba4c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-06-24 18:59:26 -0300

    feat: add eslint and base file tree configs

--- config.lua
+++ config.lua
@@ -8,12 +8,21 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Builtin Plugins
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.alpha.active                                   = true
+lvim.builtin.alpha.mode                                     = "dashboard"
+lvim.builtin.notify.active                                  = true
+lvim.builtin.terminal.active                                = true
+lvim.builtin.nvimtree.setup.view.side                       = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git         = false
+lvim.builtin.nvimtree.setup.open_on_setup                   = true
+lvim.builtin.nvimtree.setup.open_on_tab                     = true
+lvim.builtin.nvimtree.setup.open_on_setup_file              = true
+lvim.builtin.nvimtree.setup.filters.dotfiles                = false
+lvim.builtin.nvimtree.setup.view.width                      = 40
+lvim.builtin.nvimtree.setup.view.hide_root_folder           = true
+lvim.builtin.nvimtree.setup.sort_by                         = "case_sensitive"
+lvim.builtin.nvimtree.setup.view.auto_resize                = false
+lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
 
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
@@ -26,6 +35,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "tsx",
   "css",
   "rust",
+  "ruby",
   "java",
   "yaml",
 }
@@ -36,32 +46,19 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Formatters & Linters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
   {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "eslint_d",
+    command = "eslint",
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
 }
 
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    command = "eslint",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  }
+}
 
 -- Additional Plugins
 lvim.plugins = {
@@ -73,7 +70,7 @@ lvim.plugins = {
   {
     'wfxr/minimap.vim',
     run = "cargo install --locked code-minimap",
-    -- cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
     config = function()
       vim.cmd("let g:minimap_width = 10")
       vim.cmd("let g:minimap_auto_start = 1")
@@ -85,7 +82,7 @@ lvim.plugins = {
     event = "BufRead",
     config = function()
       vim.cmd "highlight default link gitblame SpecialComment"
-      vim.g.gitblame_enabled = 0
+      vim.g.gitblame_enabled = 1
     end,
   },
   {
@@ -101,32 +98,17 @@ lvim.plugins = {
     event = "WinScrolled",
     config = function()
       require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
         mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
           '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-        hide_cursor = true, -- Hide cursor while scrolling
-        stop_eof = true, -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil, -- Default easing function
-        pre_hook = nil, -- Function to run before the scrolling animation starts
-        post_hook = nil, -- Function to run after the scrolling animation ends
+        hide_cursor = true,
+        stop_eof = true,
+        use_local_scrolloff = false,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = nil,
+        pre_hook = nil,
+        post_hook = nil,
       })
     end
   },
 }
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
